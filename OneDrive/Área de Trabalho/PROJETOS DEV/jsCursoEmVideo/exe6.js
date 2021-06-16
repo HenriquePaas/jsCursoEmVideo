ff8fb442ed17f85497593780d24a65dde6e2c745
HenriquePaas <henrique.paas17@gmail.com>
2021-06-15 21:41:25 -0300

    avanço no curso

---
+++ OneDrive/Área de Trabalho/PROJETOS DEV/jsCursoEmVideo/exe6.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+
+if(vel <= 80){
+    console.log(`Sua velocidade é de ${vel} km/h`)
+    console.log(`Dirija sempre com segurança`)
+}else{
+    console.log('Você está acima da velocidade permitida. MULTADO!')
+}
---
+++ OneDrive/Área de Trabalho/PROJETOS DEV/jsCursoEmVideo/modelo/script.js
@@ -0,0 +1,18 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >= 0 && hora <12){
+        img.src = 'manha.jpg'
+        document.body.style.background = 'rgb(176, 190, 211)'
+    }else if(hora >= 12 && hora <18){
+        img.src = 'tarde.jpg'
+        document.body.style.background = 'rgb(214, 178, 110)'
+    }else{
+        img.src = 'noite.jpg'
+        document.body.style.background = 'rgb(189, 118, 189)'
+    }
+}
